805be150b42773f5945af588a82113e710837261
Mel Chang <mchang@twilio.com>
2018-05-03 14:42:38 -0700

    Add ActiveBook reducer

---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,9 @@
+// state param is state this reducer is responsible for, not neccessarily full app state
+export default function(state = null, action) {
+  switch(action.type) {
+    case 'BOOK_SELECTED':
+      return action.payload;
+  }
+
+  return state;
+}
