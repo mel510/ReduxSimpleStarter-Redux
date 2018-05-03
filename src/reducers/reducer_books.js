b22a3582d8174e5a37742b9b1d2852fa1e337830
Mel Chang <mchang@twilio.com>
2018-05-03 11:33:01 -0700

    Add BooksReducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+  return [
+    { title: 'Book1'},
+    { title: 'Book2'},
+    { title: 'Book3'},
+    { title: 'Book4'},
+  ]
+}
+
+// Reducer is a function that returns a piece of application state
